bda8a2c219eaa95835c6c002ef8303278787aed8
Adarsh Verma <adarshverma549@gmail.com>
2023-11-12 10:20:37 +0530

    adding index.js combine all the commen components with one files

---
+++ src/utils/index.js
@@ -0,0 +1,7 @@
+import Button from './Button';
+import Checkboxes from './Checkboxes';
+import Container from './Container';
+import Starts from './Starts';
+import SelectOptions from './SelectOptions';
+
+export { Button, Checkboxes, Container, Starts, SelectOptions };
