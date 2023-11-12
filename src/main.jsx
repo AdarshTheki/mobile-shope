de47a7dbc53f1a43d539354fe6da485e48015b84
Adarsh Verma <adarshverma549@gmail.com>
2023-11-12 10:06:34 +0530

    important files App.jsx, flipkart.js, index.css or main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import App from './App.jsx';
+import './index.css';
+import ProductProvider from './context/ProductProvider.jsx';
+import { CartPage, NotFound, ProductPage, ProductDetailPage, HomePage } from './pages/index.js';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/products', element: <ProductPage /> },
+      { path: '/product/:id', element: <ProductDetailPage /> },
+      { path: '/carts', element: <CartPage /> },
+      { path: '*', element: <NotFound /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ProductProvider>
+      <RouterProvider router={router} />
+    </ProductProvider>
+  </React.StrictMode>
+);
