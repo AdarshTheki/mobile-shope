9bc2f6e11afd87195b7a3a0e94470e58a7fe6094
Adarsh Verma <adarshverma549@gmail.com>
2023-11-12 10:37:14 +0530

    deleted unwanted files with src folder

--- src/utiles/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Button({ children, className = '' }) {
-  return (
-    <button className={`py-2 w-full capitalize rounded-md cursor-pointer block font-semibold ${className}`}>
-      {children}
-    </button>
-  );
-}
