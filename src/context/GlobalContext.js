13edb63b5eb415ba1e27c4a83e98b9d5aa7bc1ae
Adarsh Verma <adarshverma549@gmail.com>
2023-11-29 22:46:34 +0530

    delete files - not use properly

--- src/context/GlobalContext.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import { ContextProvider } from './GlobalProvider';
-
-// Create a custom hook to access the context
-const GlobalContext = () => {
-  return useContext(ContextProvider);
-};
-
-export default GlobalContext;
