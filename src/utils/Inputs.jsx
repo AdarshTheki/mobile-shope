9c78bf04a3d2e5ab44ab8c5f0ceb50f46f5dca90
Adarsh Verma <adarshverma549@gmail.com>
2023-12-04 23:10:34 +0530

    modify styles with inputs fields

--- src/utils/Inputs.jsx
+++ src/utils/Inputs.jsx
@@ -5,23 +5,20 @@ const Inputs = forwardRef(function InputFrom(
   ref
 ) {
   const uniqueId = useId();
+
   return (
-    <div className='w-full'>
-      {label && (
-        <label
-          htmlFor={uniqueId}
-          className='block cursor-pointer text-sm font-medium leading-6 text-gray-900'>
-          {label}
-        </label>
-      )}
-      <input
-        autoComplete='off'
-        id={uniqueId}
-        type={type}
-        {...props}
-        ref={ref}
-        className={`block w-full rounded-md border p-2 mt-2 border-gray-300 shadow-md focus:outline-gray-700 sm:text-sm ${className}`}
-      />
+    <div className='block'>
+      <label htmlFor={uniqueId}>
+        <span className='block text-sm font-medium text-slate-700'>{label}</span>
+        <input
+          autoComplete='off'
+          id={uniqueId}
+          type={type}
+          {...props}
+          ref={ref}
+          className={`mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-m text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 ${className}`}
+        />
+      </label>
     </div>
   );
 });
