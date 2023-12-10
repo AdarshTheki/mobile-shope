c9e3bf3b75d76689ce325e14129a1af05801d424
Adarsh Verma <adarshverma549@gmail.com>
2023-12-10 09:24:38 +0530

    Product Filters Components - modify checkboxes likes, category, display, camera & more...

---
+++ src/components/Product/filters/FilterComponent.jsx
@@ -0,0 +1,97 @@
+import React, { useId, useMemo } from 'react';
+import { useFilter } from '../../../context/Filter_Context';
+
+// eslint-disable-next-line react/prop-types
+export default function FilterComponent({ name, label, items }) {
+  const { all_products, toggleCheckbox, filters } = useFilter();
+
+  const RemoveFilterButtons = ({ arr }) => {
+    return (
+      <button
+        key={arr}
+        onClick={() => toggleCheckbox(name, arr)}
+        className='bg-red-200 text-red-600 hover:bg-red-300 duration-300 text-xs py-1 px-2 font-medium rounded-lg'>
+        {arr.toString().slice(0, 8)} {label} ×
+      </button>
+    );
+  };
+
+  const CheckBoxes = ({ value }) => {
+    const names = name?.toLowerCase();
+    const Id = useId();
+    const totalItems = useMemo(
+      () => all_products.filter((i) => i[names] === value),
+      [value, names]
+    );
+    return (
+      <label
+        htmlFor={Id}
+        key={value}
+        className='flex gap-1.5 items-center flex-wrap cursor-pointer hover:text-blue-500 duration-200'>
+        <input
+          type='checkbox'
+          id={Id}
+          checked={filters[`selected${name}`]?.includes(value)}
+          onChange={() => toggleCheckbox(name, value)}
+        />
+        <span className=' capitalize'>{value.toString().slice(0, 8)}</span>
+        <span className='normal-case'>{label}</span>
+        <span className='text-gray-400 text-sm'>({totalItems?.length})</span>
+      </label>
+    );
+  };
+
+  const ClearAllButton = ({ name }) => {
+    const { filters, clearCategory, clearCamera, clearDisplay, clearRam, clearBattery } =
+      useFilter();
+
+    const allCheckboxClear = async () => {
+      if (name === 'Category') {
+        await clearCategory();
+      }
+      if (name === 'Battery') {
+        await clearBattery();
+      }
+      if (name === 'Ram') {
+        await clearRam();
+      }
+      if (name === 'Display') {
+        await clearDisplay();
+      }
+      if (name === 'Camera') {
+        await clearCamera();
+      }
+    };
+    return (
+      <div className='flex justify-between pb-1'>
+        <h2 className='text-gray-600 font-medium uppercase'>{name}</h2>
+        {filters[`selected${name}`]?.length !== 0 && (
+          <button onClick={allCheckboxClear} className='text-xs mr-3 font-medium text-red-500'>
+            Clear All
+          </button>
+        )}
+      </div>
+    );
+  };
+
+  return (
+    <div className='bg-white pl-4 rounded-lg py-2 text-gray-700 text-sm'>
+      {/* Remove all Filter Buttons */}
+      <ClearAllButton name={name} />
+
+      {/* Remove Filter Buttons */}
+      <div className=' flex flex-wrap gap-2'>
+        {filters[`selected${name}`]?.map((i) => (
+          <RemoveFilterButtons key={i} arr={i} />
+        ))}
+      </div>
+
+      {/* Display Checkboxes */}
+      <div>
+        {items.map((item) => (
+          <CheckBoxes key={item} value={item} />
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Product/filters/FilterRatings.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { useFilter } from '../../../context/Filter_Context';
+import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
+
+export default function FilterRatings() {
+  const { filters, all_products, updateFilters } = useFilter();
+  const { stars } = filters;
+
+  const Stars = ({ star }) => {
+    const tempStar = Array.from({ length: 5 }, (_, index) => {
+      const number = index + 0.5;
+      return (
+        <span key={index}>
+          {star > number ? <BsStarFill /> : star > index ? <BsStarHalf /> : <BsStar />}
+        </span>
+      );
+    });
+    return (
+      <div>
+        <span className='flex text-yellow-600 space-x-1 text-sm'>{tempStar}</span>
+      </div>
+    );
+  };
+
+  const RadioInputs = ({ value }) => {
+    let totalItems = all_products.filter((i) => Math.round(i.stars) === value);
+    return (
+      <div className='space-x-3 flex'>
+        <input
+          type='radio'
+          name='stars'
+          checked={stars === value}
+          id={value}
+          value={value}
+          onChange={updateFilters}
+        />
+        <label htmlFor={value} className='flex cursor-pointer text-xs items-center gap-1'>
+          <Stars star={value} /> <span className='text-gray-400'>({totalItems.length})</span>
+        </label>
+      </div>
+    );
+  };
+
+  return (
+    <div className='bg-white pl-4 py-2 rounded-lg'>
+      <h2 className='uppercase text-sm text-gray-600'>Ratings</h2>
+      <div className='space-x-3 flex'>
+        <input
+          type='radio'
+          name='stars'
+          checked={stars === 0}
+          id='allStars'
+          value={0}
+          onChange={updateFilters}
+        />
+        <label htmlFor='allStars' className='flex cursor-pointer text-xs items-center gap-1'>
+          all
+        </label>
+      </div>
+      {[1, 2, 3, 4, 5].map((i) => (
+        <RadioInputs key={i} value={i} />
+      ))}
+    </div>
+  );
+}
