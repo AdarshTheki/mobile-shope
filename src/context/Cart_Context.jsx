bdf1da8a783255746974e372bf3a4c9fd716b4a7
Adarsh Verma <adarshverma549@gmail.com>
2023-12-14 17:23:39 +0530

    Context API - Auth, Cart and Review with multiple featured added

--- src/context/Cart_Context.jsx
+++ src/context/Cart_Context.jsx
@@ -7,31 +7,52 @@ import {
   INCREASE_CART_QUANTITY,
   DECREASE_CART_QUANTITY,
   COUPON_USE_TOTALS,
+  CHANGE_ADDRESS,
 } from '../action';
 import reducers from '../reducers/cart_reducer';
 
-const getLocalStorage = () => {
-  let cart = localStorage.getItem('cart');
-  if (cart) {
-    return JSON.parse(localStorage.getItem('cart'));
-  } else {
-    return [];
-  }
+const data = [
+  {
+    id: 7,
+    name: 'APPLE iPhone 12 (Black, 64 GB)',
+    price: 40999,
+    amount: 1,
+    url: 'https://rukminim2.flixcart.com/image/312/312/kg8avm80/mobile/r/h/z/apple-iphone-12-dummyapplefsn-original-imafwg8duby8qbn4.jpeg?q=70',
+  },
+  {
+    id: 184,
+    name: 'Infinix HOT 30 5G (Knight Black, 128 GB)',
+    price: 12499,
+    amount: 2,
+    url: 'https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/i/w/x/hot-30-5g-x6832-infinix-original-imagrfwzw66ubgnk.jpeg?q=70',
+  },
+];
+
+const detail = {
+  name: 'Adarsh Verma',
+  address:
+    'ARAMARK Ltd 30 Commercial Road Fratton PORTSMOUTH Hampshire usually comes last. In much of Europe,',
+  phone: '539982345',
+  city: 'Bhutan',
+  country: 'Indonashia',
+  postal_code: '4422100',
 };
 
 const initialState = {
-  cart: getLocalStorage(),
+  cart: data,
   total_items: 0,
   total_amount: 0,
   coupon_amount: 0,
   coupon_code: '',
   shipping_fee: 180,
+  address: detail,
 };
 
 export const CartContext = React.createContext();
 
 export const CartProvider = ({ children }) => {
   const [state, dispatch] = React.useReducer(reducers, initialState);
+
   // add to cart
   const addToCart = (item) => {
     dispatch({ type: ADD_TO_CART, payload: item });
@@ -56,9 +77,12 @@ export const CartProvider = ({ children }) => {
   const couponCode = (coupon) => {
     dispatch({ type: COUPON_USE_TOTALS, payload: { coupon } });
   };
+  // change Address
+  const changeAddress = (item) => {
+    dispatch({ type: CHANGE_ADDRESS, payload: item });
+  };
 
   React.useEffect(() => {
-    localStorage.setItem('cart', JSON.stringify(state.cart));
     dispatch({ type: COUNT_CART_TOTALS });
   }, [state.cart]);
 
@@ -72,6 +96,7 @@ export const CartProvider = ({ children }) => {
         increaseQty,
         decreaseQty,
         couponCode,
+        changeAddress,
       }}>
       {children}
     </CartContext.Provider>
